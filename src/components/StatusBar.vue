9ef15b99421f892bbc487d96dd6e9f09d4f4d05c
kissmygritts <kissmygritts08@gmail.com>
2018-06-14 18:02:54 -0700

    Style: status bar size and styling

--- src/components/StatusBar.vue
+++ src/components/StatusBar.vue
@@ -2,7 +2,8 @@
   <nav class="navbar">
     <div class="navbar-brand">
       <a href="#" class="navbar-item">
-        <h1 class="is-size-5 is-capitalized">{{ $route.name }}</h1>
+        <!-- <h1 class="is-size-5 is-capitalized">{{ $route.name }}</h1> -->
+        <h1 class="f3 main fw3 ttc ma1 pa0 lh-title">{{ $route.name }}</h1>
       </a>
 
       <a href="#" class="navbar-burger">
